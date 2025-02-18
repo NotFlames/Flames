c89ccc7b0e83f33398d7244067f7c2f9ffb76ffe
NotFlames <bharathimoorthy25@gmail.com>
2025-02-19 01:41:34 +0530

    Update and rename index.html to config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# Basic User Bot Requirements
+API_ID = 3769190
+API_HASH = "e125d5abf9dadd0f9b861f774f5aae6a"
+PHONE = +918248516714
+
+# Parameters
+INSTA_USERNAME = "alqabas"
+SCARP_GRP = "beuqk"
+CHANNEL_TO_POST = "yeayeahb"
+NO_OF_PINNED = 3
+TIME_BET_REQ = 1800
+
+# Proxies
+PROXIES =[
+    "dpngjpzu:ko1phn78hi3o@198.23.239.134:6540",
+    "dpngjpzu:ko1phn78hi3o@207.244.217.165:6712",
+    "dpngjpzu:ko1phn78hi3o@173.0.9.70:5653"
+]
